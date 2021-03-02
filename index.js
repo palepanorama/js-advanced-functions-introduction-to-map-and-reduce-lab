401666fe883d84c468d5e97a52404ef9a44cc572
palepanorama <matthew.keith.thomas@gmail.com>
2021-03-02 15:04:46 -0600

    commit tree c0eaa0639dcb55fd0e965cc453e7ed1e33ef9839

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+function mapToNegativize(arr){
+    let newArr = arr.map(x => x * -1);
+    return newArr;
+}
+
+function mapToNoChange(arr){
+    let newArr = arr.map(x => x);
+    return newArr;
+}
+
+function mapToDouble(arr){
+    let newArr = arr.map(x => x * 2);
+    return newArr;
+}
+
+function mapToSquare(arr){
+    let newArr = arr.map(x => x**2);
+    return newArr;
+}
+
+function reduceToTotal(arr, startingPoint= 0){
+    let total = startingPoint;
+    for (let i = 0; i < arr.length; i++) {
+        total = total +arr[i];
+    }
+    return total;
+}
+
+function reduceToAllTrue(arr){
+    for (let i = 0; i < arr.length; i++){
+        if (!arr[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+function reduceToAnyTrue(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (!!arr[i]) {
+            return true;
+        }
+    }
+    return false;
+}
